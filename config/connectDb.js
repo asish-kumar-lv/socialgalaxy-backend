395ae91ce966d0ea0cb6659ad067535c58f72c3c
asish-kumar-lv <asish.kumar@letsventure.com>
2024-12-03 00:58:03 +0530

    dbname change

--- config/connectDb.js
+++ config/connectDb.js
@@ -6,7 +6,8 @@ const connectDb = async (DB_URL) => {
       dbName: "socialGalaxy",
     };
     await mongoose.connect(
-      "mongodb+srv://asishkumar:admin%406969@cluster0.ayipn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+      "mongodb+srv://asishkumar:admin%406969@cluster0.ayipn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+      DB_OPTIONS
     );
     console.log("conncted successfully");
   } catch (e) {
